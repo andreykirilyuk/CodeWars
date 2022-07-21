fbb7be6d2ce347bbbd1193c4f46a2a8964c9a6d1
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-07-20 22:55:52 -0400

    8kyu includes method

---
+++ You_only_need_one_-_Beginner.js
@@ -0,0 +1,12 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+//Answer
+function check(a, x) {
+    return a.includes(x)
+  }
+
+// have to remember that includes() is the method for checking containing values in array
