00020e8cf3a9ad29bf1c8e5cc05e8b47e38657ee
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-07-29 01:27:09 -0400

    7kyu flatten and sort array solved

--- Flatten_and_sort_an_array.js
+++ Flatten_and_sort_an_array.js
@@ -5,3 +5,12 @@
 // Example:
 
 // Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+//Answer
+"use strict";
+
+function flattenAndSort(array) {
+  return array.reduce((acc,c) => acc.concat(c),[]).sort((a,b) => a-b)
+}
+
+//we reduce the array by using the concatenating method to join the accumulating array with the current one, starting at an empty array [], then sort them into ascending order.
