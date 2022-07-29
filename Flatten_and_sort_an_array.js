bdaab7ebe52c5bd7a32cb6aac0459cae42fbfe71
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-07-29 01:28:30 -0400

    7kyu flatten and sorted explained

--- Flatten_and_sort_an_array.js
+++ Flatten_and_sort_an_array.js
@@ -13,4 +13,4 @@ function flattenAndSort(array) {
   return array.reduce((acc,c) => acc.concat(c),[]).sort((a,b) => a-b)
 }
 
-//we reduce the array by using the concatenating method to join the accumulating array with the current one, starting at an empty array [], then sort them into ascending order.
+//we reduce the array by using the concatenating method to join the accumulating array with the current one, starting at an empty array [], then sort the resulting flat array into ascending order.
