f90e01548a9183ec588c0720ce835c07612af636
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-02 00:05:03 -0400

    8kyu multiplication alternate answer

--- 8kyu_Simple_multiplication.js
+++ 8kyu_Simple_multiplication.js
@@ -8,3 +8,9 @@ function simpleMultiplication(number) {
     }else return number*9
  }
 //check if num modulus 2 is zero then multiply by 8, else multiply by 9
+
+//alternate one liner answer:
+function simpleMultiplication(number) {
+    return number * (number%2 ? 9 : 8)
+}
+//ternary operator to check if mod2 !== 0, multiply by 9 else by 8
