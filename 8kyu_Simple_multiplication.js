a2c8b90eb706c203bc59cfa9ba16517b8fba61a1
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-02 00:02:49 -0400

    8kyu multiplication solved

--- 8kyu_Simple_multiplication.js
+++ 8kyu_Simple_multiplication.js
@@ -1,2 +1,10 @@
 /*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
+
+//Answer
+function simpleMultiplication(number) {
+    if(number%2 === 0){
+      return number*8
+    }else return number*9
+ }
+//check if num modulus 2 is zero then multiply by 8, else multiply by 9
