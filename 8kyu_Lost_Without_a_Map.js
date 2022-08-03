16e5adc45a3be6584979673cf672d9967af45085
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-03 00:43:16 -0400

    8kyu mapping solved

---
+++ 8kyu_Lost_Without_a_Map.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+//Answer
+function maps(x){
+    return x.map(x => x*2)
+  }
+//map with values of x doubled
