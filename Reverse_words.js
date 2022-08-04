26689fed6f5f7022dd80fa496eaeb1a6a4cded38
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-04 01:14:53 -0400

    7kyu reverse string answer

--- Reverse_words.js
+++ Reverse_words.js
@@ -5,3 +5,14 @@ Examples
 "This is an example!" ==> "sihT si na !elpmaxe"
 "double  spaces"      ==> "elbuod  secaps"
 */
+
+//Answer
+function reverseWords(str) {
+    let split = str.split(' ')
+    for(let i = 0; i < split.length; i++){
+      split[i] = split[i].split('').reverse().join('')
+    }
+    return split.join(' ')
+  }
+
+//do a split and reverse looping through the array, just being careful with the split parameters
