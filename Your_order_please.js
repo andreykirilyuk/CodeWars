56603d44eaef1ce7714422c5fd75559954cc1067
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-05 00:01:01 -0400

    6kyu ordering

--- Your_order_please.js
+++ Your_order_please.js
@@ -10,3 +10,8 @@ Examples
 "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 ""  -->  ""
 */
+
+//Answer
+function order(words){
+    return words.split(' ').sort((a,b) => a.match(/\d/) - b.match(/\d/)).join(' ')
+  }
