c7b157dc7262fe5e471afed57fdd3f197452cbd2
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-06 00:17:49 -0400

    8kyu quarter solved

--- Quarter_of_the_year.js
+++ Quarter_of_the_year.js
@@ -2,3 +2,15 @@
 
 For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 */
+
+//Answer
+const quarterOf = (month) => {
+    if(month <= 3){
+      return 1
+    }else if(month <= 6){
+      return 2
+    }else if(month <= 9){
+      return 3
+    }else return 4
+   }
+//simple conditional just be careful to do <= not <
