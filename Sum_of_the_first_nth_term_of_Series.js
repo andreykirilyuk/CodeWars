28b6612ffae71d3aa64c4ee32271dbeb3849d315
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-07 01:34:46 -0400

    7kyu series summation solved

--- Sum_of_the_first_nth_term_of_Series.js
+++ Sum_of_the_first_nth_term_of_Series.js
@@ -15,3 +15,13 @@ Examples:(Input --> Output)
 2 --> 1 + 1/4 --> "1.25"
 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
 */
+
+//Answer
+function SeriesSum(n)
+{
+  for(var s = 0, i = 0; i < n; i++){
+    s += 1 / (1 + i * 3 )
+  }
+  return s.toFixed(2)
+}
+//declare variable s to sum the series together, use i to iterate the denominators, and use toFixed(2) for the decimal places.
