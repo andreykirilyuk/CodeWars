0d61868ce2f7caf45509c020e06ec3df38ae62ce
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-10 00:02:02 -0400

    8kyu area or perimeter solved

--- Area_or_perimeter.js
+++ Area_or_perimeter.js
@@ -8,3 +8,10 @@ Example(Input1, Input2 --> Output):
 3, 3 --> 9
 Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 */
+
+//Answer
+const areaOrPerimeter = function(l , w) {
+    if(l === w){
+      return Math.pow(l,2)
+    }else return (l+w)*2
+   };
