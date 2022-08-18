168fdc6be77176e703e43551eb56ddcf1fa8104a
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-18 00:16:18 -0400

    answered

--- 8kyu_find_the_smallest_integer_in_the_array.js
+++ 8kyu_find_the_smallest_integer_in_the_array.js
@@ -7,3 +7,11 @@ Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.
 */
+
+//Answer
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let sorted = args.sort((a,b) => a-b)
+      return sorted[0]
+    }
+  }
