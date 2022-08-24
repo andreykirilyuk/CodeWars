9ef1e0c80158c7a279be56b4a492782f6e51f3b2
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-24 00:21:50 -0400

    explained

--- 8kyu_Grasshopper_PersonalizedMessage.js
+++ 8kyu_Grasshopper_PersonalizedMessage.js
@@ -12,3 +12,9 @@ otherwise	'Hello guest'
 function greet (name, owner) {
     return 'Hello ' + (name === owner ? 'boss' : 'guest')
   }
+
+//do a ternary check to see what to concatencate after hello, but you can also do this in many ways i.e.
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest'
+  }
+//this gives it a clearer return I suppose
