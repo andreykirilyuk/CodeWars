19b735f31ad6ef4d27bf0bd14c91955525d4fa09
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-25 02:29:10 -0400

    solved

--- 8kyu_CorrectTheMistakes.js
+++ 8kyu_CorrectTheMistakes.js
@@ -10,3 +10,9 @@ O is misinterpreted as 0
 I is misinterpreted as 1
 The test cases contain numbers only by mistake.
 */
+
+//Answer
+function correct(string)
+{
+  return string.split('0').join('O').split('5').join('S').split('1').join('I')
+}
