24e585207eec62361045867e42fd653a8dc17b04
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-08-30 00:26:27 -0400

    solved

--- 8kyu_NthPower.js
+++ 8kyu_NthPower.js
@@ -8,3 +8,10 @@ Let's look at a few examples:
 array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 */
+
+//Answer
+function index(array, n){
+    if(!array[n]){
+      return -1
+    }else return Math.pow(array[n],n)
+  }
