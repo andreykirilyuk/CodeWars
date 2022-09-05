bb7647d09c6c57aec50e17169fec8c39903080bf
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-09-04 21:57:26 -0400

    solved

--- 7kyu_Isograms.js
+++ 7kyu_Isograms.js
@@ -7,3 +7,16 @@ Example: (Input --> Output)
 "aba" --> false
 "moOse" --> false (ignore letter case)
 */
+
+//Answer
+function isIsogram(str){
+    str = str.toLowerCase()
+    for(let i = 0; i < str.length; i++){
+      for(let j = i+1; j < str.length; j++){
+        if (str[i] === str[j]){
+          return false
+        }
+      }
+    }
+    return true
+  }
