1c8e79fa6ae75271b02bb5030d274b86b5c0d83b
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-09-10 00:21:16 -0400

    explained

--- 8kyu_UnderPressure.js
+++ 8kyu_UnderPressure.js
@@ -6,3 +6,6 @@ Code as fast as you can! You need to double the integer and return it.
 function doubleInteger(i) {
     return i*2;
   }
+
+//Explained
+//just return the integer using math operator * 2
