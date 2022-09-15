919be9f5968e72757fdbf37aba5271452e78c34d
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-09-14 21:39:21 -0400

    solved

--- 8kyu_TwiceAsOld.js
+++ 8kyu_TwiceAsOld.js
@@ -5,3 +5,9 @@ current father's age (years)
 current age of his son (years)
 Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 */
+
+//Answer
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let x = dadYearsOld - (sonYearsOld*2)
+    return Math.abs(x)
+  }
