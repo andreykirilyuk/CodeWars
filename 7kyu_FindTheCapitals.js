f5ab623e365ce39d2a0377456345666c672b4e42
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-09-19 23:54:15 -0400

    solved

--- 7kyu_FindTheCapitals.js
+++ 7kyu_FindTheCapitals.js
@@ -5,3 +5,15 @@ Write a function that takes a single string (word) as argument. The function mus
 Example
 Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
+
+//Answer
+var capitals = function (word) {
+    let result = []
+      let arr = word.split('')
+    for(let i = 0; i < arr.length; i++){
+      if (arr[i] === arr[i].toUpperCase()) {
+        result.push(i)
+      } 
+    }
+    return result
+  };
