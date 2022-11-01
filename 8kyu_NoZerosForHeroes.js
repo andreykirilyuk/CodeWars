5027bae74fa0959fbb295bddcddb1cb192969342
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-11-01 00:38:36 -0400

    solved

--- 8kyu_NoZerosForHeroes.js
+++ 8kyu_NoZerosForHeroes.js
@@ -10,3 +10,10 @@ Get rid of them. Only the ending ones.
 -1050 -> -105
 Zero alone is fine, don't worry about it. Poor guy anyway
 */
+
+//Answer
+function noBoringZeros(n) {
+    if(n === 0) return n
+    if(n % 10 === 0) return noBoringZeros (n/10)
+    else return n
+  }
