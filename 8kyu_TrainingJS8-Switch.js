f729ed8e998dc180537527d1f051c99d77f59b6b
Andrey Kirilyuk <andre.kirilyuk@gmail.com>
2022-11-17 21:19:16 -0500

    solved

--- 8kyu_TrainingJS8-Switch.js
+++ 8kyu_TrainingJS8-Switch.js
@@ -65,3 +65,29 @@ Tip: Using default for most of the cases can reduce your work.
 
 When you have finished, click "Test" to test your code against initial tests and "Attempt" to test your code against all tests. If you pass in all tests you can click "Submit" to submit your code.
 */
+
+//Solved
+function howManydays(month){
+    var days;
+    switch (month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+          return 31
+          break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+          return 30
+          break;
+        case 2:
+          return 28
+    
+    }
+    return days;
+  }
